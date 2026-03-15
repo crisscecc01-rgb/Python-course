6cf22ab7202382704ab2306b9217c39d14cc8efe
crisscecc01-rgb <crisscecc.01@gmail.com>
2026-03-15 18:57:58 +0100

    Add files via upload

--- PKGame.py
+++ PKGame.py
@@ -1,40 +1,49 @@
 from PkClasses import *
 
+#creates a playable pokémon
 def create_playable_pokemon(base, level):
     stats_copy = dict(base.base_stats)
     current_hp = int(stats_copy["hp"])
-    move_objects = [MovesList[name] for name in base.moves]
+    move_objects = [MovesList[name].move_copy() for name in base.moves]
     return PokemonCharacterClass(
         name= base.name,
         level=level,
         types=base.types,
         stats=stats_copy,
         moves=move_objects,
+        modifiers={
+        "attack": 0,
+        "defense": 0,
+        "speed": 0,
+        "special": 0
+    },
         currentHP=current_hp
     )
 
+#function to select a move
 def move_selection(userPk):
     print("Select a move:")
     i = 0
     for move in userPk.moves:
         i = i + 1
-        print(str(i) + ")", move.name)
+        print(str(i) + ")", move.name, " pp: ", move.pp)
     choice = input("> ")
     while not choice.isdigit() or int(choice) < 1 or int(choice) > len(userPk.moves):
         print("Select between 1 and " + str(len(userPk.moves)) + ":")
         i = 0
         for move in userPk.moves:
             i = i + 1
-            print(str(i) + ")", move.name)
+            print(str(i) + ")", move.name, " pp: ", move.pp)
         choice = (input("> "))
     return userPk.moves[int(choice) - 1]
 
-
+#shows a basic HUD of the battle
 def show_HUD(UserPk, opponent):
     print(f"{opponent.name} current hp: {opponent.currentHP}")
     print(f"{UserPk.name}  current hp: {UserPk.currentHP}")
     return move_selection(UserPk)
 
+#function which is used to generate a single battle turn. if the user loose it returns 2, if the user wins it returs 1, otherwise it returns 0
 def pk_battle(trainer, enemy, UserPkIndex, EnemyPkIndex):
     selected_move = show_HUD(trainer.pk_list[UserPkIndex], enemy.pk_list[EnemyPkIndex])
     # speed check
@@ -70,6 +79,6 @@ def pk_battle(trainer, enemy, UserPkIndex, EnemyPkIndex):
                 print(f"{enemy.pk_list[EnemyPkIndex].name} has no HP left")
                 return 1
             else:
-
+                print(f"{enemy.pk_list[EnemyPkIndex].name} has {enemy.pk_list[EnemyPkIndex].currentHP} left")
                 return 0
 
--- PkClasses.py
+++ PkClasses.py
@@ -24,17 +24,21 @@ class PokemonTrainerClass:
 
 
 class PokemonCharacterClass:
-    def __init__(self, name, level, types, stats, moves, currentHP):
+    def __init__(self, name, level, types, stats, modifiers, moves, currentHP):
         self.name = name
         self.level = level
         self.types = types
         self.stats = stats
+        self.modifiers = modifiers
         self.moves = moves
         self.currentHP = currentHP
 
-    def use_move(self, move, opponent, ):
 
+# function used to apply status or damage from a move
+    def use_move(self, move, opponent, ):
+        #check if the move is in the move pool
         if move in self.moves:
+            #reduces the pp of the move
             if move.pp > 0:
                 print(f"{self.name} uses {move.name} against {opponent.name}!")
                 move.pp -= 1
@@ -45,33 +49,78 @@ class PokemonCharacterClass:
         if random.random() > move.accuracy:
             print("\nBut it failed!")
             return
+        #check if the move is a status move or a damage move
+        if move.category == "status":
+            modifierIndex = 0
+            #check the target of the status move
+            if move.effect["target_stat"][0] == "self":
+                #modification of the modifiers attribute
+                for stat in move.effect["target_stat"][1:]:
+                    #check the boundaries
+                    if self.modifiers[stat] > 5:
+                        print(f"{stat} of {self.name} can't increase any further!")
+                    elif self.modifiers[stat] < -5:
+                        print(f"{stat} of {self.name} can't decrease any further!")
+                    else:
+                        #apply the status
+                        self.modifiers[stat] = self.modifiers[stat] + move.effect["change"][modifierIndex]
+                        
+                        if move.effect["change"][modifierIndex] > 0:
+                            print(f"{stat} of {self.name} increased!")
+                        else:
+                            print(f"{stat} of {self.name} decreased!")
+                        modifierIndex = modifierIndex + 1
+            #same procedure in case the target is the opponent
+            else:
 
-        # Choose correct attack/defense stat
-        if move.category == "physical":
-            attack = self.stats["attack"]
-            defense = opponent.stats["defense"]
+                for stat in move.effect["target_stat"][1:]:
+                    if opponent.modifiers[stat] > 5:
+                        print(f"{stat} of {opponent.name} can't increase any further!")
+                    elif opponent.modifiers[stat] < -5:
+                        print(f"{stat} of {opponent.name} can't decrease any further!")
+                    else:
+                        opponent.modifiers[stat] = opponent.modifiers[stat] + move.effect["change"][modifierIndex]
+                        if move.effect["change"][modifierIndex] > 0:
+                            print(f"{stat} of {opponent.name} increased!")
+                        else:
+                            print(f"{stat} of {opponent.name} decreased!")
+                        modifierIndex = modifierIndex + 1
+
         else:
-            attack = self.stats["special"]
-            defense = opponent.stats["special"]
-        modifier, is_crit, effectiveness = self.calc_modifier(move, opponent)
-        # Damage formula
-        base = ((2 * self.level + 10) / 250) * (attack / defense) * move.power + 2
-        damage = int(base * modifier)
+            # Choose correct attack/defense stat
+            if move.category == "physical":
+                attack = self.get_modified_stat("attack")
+                defense = opponent.get_modified_stat("defense")
+            else:
+                attack = self.get_modified_stat("special")
+                defense = opponent.get_modified_stat("special")
+            modifier, is_crit, effectiveness = self.calc_modifier(move, opponent)
+            # Damage formula
+            base = ((2 * self.level + 10) / 250) * (attack / defense) * move.power + 2
+            damage = int(base * modifier)
 
-        # Apply damage
-        opponent.currentHP = max(0, opponent.currentHP - damage)
+            # Apply damage
+            opponent.currentHP = max(0, opponent.currentHP - damage)
 
-        # Messages
-        if is_crit:
-            print("critical hit!")
-        if effectiveness == 0.5 :
-            print(f"{move} is NOT MUCH EFFECTIVE on {opponent.name}!")
-        elif effectiveness == 2.0 :
-            print(f"{move} is SUPER EFFECTIVE on {opponent.name}!")
-        print(f"{opponent.name} takes {damage} HP damage.")
-        print(f"HP left of {opponent.name}: {opponent.currentHP}")
+            # Messages
+            if is_crit:
+                print("critical hit!")
+            if effectiveness == 0.5 :
+                print(f"{move} is NOT MUCH EFFECTIVE on {opponent.name}!")
+            elif effectiveness == 2.0 :
+                print(f"{move} is SUPER EFFECTIVE on {opponent.name}!")
+            print(f"{opponent.name} takes {damage} HP damage.")
+            print(f"HP left of {opponent.name}: {opponent.currentHP}")
+
+    #get the modified stats from the applied status to the Pk
+    def get_modified_stat(self, stat):
+        if self.modifiers[stat] >= 0:
+            return self.stats[stat]*(self.modifiers[stat] + 2)/2
+        else:
+            return self.stats[stat]*(2)/(2 - self.modifiers[stat])
 
 
+    # evaluate the total modifiers for the damage evaluation
     def calc_modifier(self, move, opponent):
         stab = self.calc_stab(move)
         effectiveness = self.calc_effectiveness(move, opponent)
@@ -80,10 +129,11 @@ class PokemonCharacterClass:
         modifier = stab*effectiveness*crit*luck
         return modifier, is_crit, effectiveness
 
-
+    #evaluate the STAB (same type attack boost)
     def calc_stab(self, move):
         return 1.5 if move.type in self.types else 1.0
 
+    #check if the move is super effective or not
     @staticmethod
     def calc_effectiveness(move, opponent):
         eff = 1.0
@@ -92,47 +142,123 @@ class PokemonCharacterClass:
                 if t in TYPE_CHART[move.type]:
                     eff = eff*TYPE_CHART[move.type][t]
                 else:
-                    print(f"Opponent type ({opponent.types}) in not registered for effectiveness evaluation")
+                    #print(f"Opponent type ({opponent.types}) in not registered for effectiveness evaluation")
                     eff*= 1
             else:
-                print(f"Chosen move type ({move.type}) is not registered for effectiveness evaluation")
+                #print(f"Chosen move type ({move.type}) is not registered for effectiveness evaluation")
                 eff*= 1
         return eff
-
+    #random critical hit, based on the user speed
     def calc_critical(self):
         crit_chance = self.stats["speed"] / 512
         is_crit = random.random() < crit_chance
         return (2.0 if is_crit else 1.0), is_crit
-
+    #random roll between 0,85 and 1
     @staticmethod
     def calc_luck():
         return random.uniform(0.85, 1.0)
 
 
-
+#class used to define a move
 class Move:
-    def __init__(self, name, move_type, category, power, accuracy, pp):
+    def __init__(self, name, move_type, category, power, accuracy, pp, effect):
         self.name = name
         self.type = move_type
         self.category = category
         self.power = power
         self.accuracy = accuracy
         self.pp = pp
+        self.effect = effect
+
+    def move_copy(self):
+        return Move(
+            self.name,
+            self.type,
+            self.category,
+            self.power,
+            self.accuracy,
+            self.pp,
+            effect=self.effect.copy() if self.effect else None
+        )
 
 
 
-TYPE_CHART = { "fire" : {"fire" : 0.5, "water" : 2, "grass" : 2 } ,
- "grass" : {"fire" : 0.5, "water" : 2, "grass" : 0.5 },
- "water" : {"fire" : 2, "water" : 0.5, "grass" : 0.5 } }
-
-
-
-MovesList = {
-    "tackle": Move("tackle", "normal", "physical", 35, 0.5, 35),
-    "razor leaf": Move("razor leaf", "grass", "physical", 55, 0.95, 25),
-    "ember": Move("ember", "fire", "special", 40, 1.0, 25),
-    "water gun": Move("water gun", "water", "special", 40, 0.5, 25)
+#list of effectivness, every type has its effective dictionary associated
+TYPE_CHART = {
+    "normal": {
+        "rock": 0.5, "ghost": 0, "steel": 0  
+    },
+    "fire": {
+        "fire": 0.5, "water": 0.5, "grass": 2, "ice": 2, "bug": 2,
+        "rock": 0.5, "dragon": 0.5
+    },
+    "water": {
+        "fire": 2, "water": 0.5, "grass": 0.5, "ground": 2,
+        "rock": 2, "dragon": 0.5
+    },
+    "grass": {
+        "fire": 0.5, "water": 2, "grass": 0.5, "poison": 0.5,
+        "ground": 2, "flying": 0.5, "bug": 0.5, "rock": 2,
+        "dragon": 0.5
+    },
+    "electric": {
+        "water": 2, "grass": 0.5, "electric": 0.5,
+        "ground": 0, "flying": 2, "dragon": 0.5
+    },
+    "ice": {
+        "fire": 0.5, "water": 0.5, "ice": 0.5,
+        "grass": 2, "ground": 2, "flying": 2, "dragon": 2
+    },
+    "fighting": {
+        "normal": 2, "ice": 2, "rock": 2,
+        "poison": 0.5, "flying": 0.5, "psychic": 0.5,
+        "bug": 0.5, "ghost": 0
+    },
+    "poison": {
+        "grass": 2, "poison": 0.5, "ground": 0.5,
+        "rock": 0.5, "ghost": 0.5
+    },
+    "ground": {
+        "fire": 2, "electric": 2, "grass": 0.5,
+        "poison": 2, "flying": 0, "bug": 0.5,
+        "rock": 2
+    },
+    "flying": {
+        "grass": 2, "electric": 0.5, "fighting": 2,
+        "bug": 2, "rock": 0.5
+    },
+    "psychic": {
+        "fighting": 2, "poison": 2,
+        "psychic": 0.5
+    },
+    "bug": {
+        "grass": 2, "psychic": 2,
+        "fire": 0.5, "fighting": 0.5, "poison": 2,
+        "flying": 0.5, "ghost": 0.5
+    },
+    "rock": {
+        "fire": 2, "ice": 2, "flying": 2, "bug": 2,
+        "fighting": 0.5, "ground": 0.5
+    },
+    "ghost": {
+        "ghost": 2, "psychic": 0  
+    },
+    "dragon": {
+        "dragon": 2
+    }
 }
+
+#defined moves, only damage moves and status moves
+MovesList = {
+    "tackle": Move("tackle", "normal", "physical", 35, 0.95, 35, None),
+    "razor leaf": Move("razor leaf", "grass", "physical", 55, 0.95, 25, None),
+    "ember": Move("ember", "fire", "special", 40, 1.0, 25, None),
+    "water gun": Move("water gun", "water", "special", 40, 1.0, 25, None),
+    "growl": Move("growl", "normal", "status", None, 1.0, 40, effect={"target_stat": ["opponent","attack",], "change": [-1,]}),
+    "tail wip": Move("tail wip", "normal", "status", None, 1.0, 40, effect={"target_stat": ["opponent", "defense",], "change": [-1,]}),
+    "growth": Move("growth", "normal", "status", None, 1.0, 40, effect={"target_stat": ["self", "attack", "defense"], "change": [+1, +1]})
+}
+
 
 BulbasaurBase = PokemonBase(
     pokedex_number = 1,
@@ -145,7 +271,7 @@ BulbasaurBase = PokemonBase(
         "speed": 45,
         "special": 65
     }),
-    moves=("tackle", "razor leaf")
+    moves=("tackle", "razor leaf", "growl", "growth")
 )
 CharmanderBase = PokemonBase(
     pokedex_number=4,
@@ -158,7 +284,7 @@ CharmanderBase = PokemonBase(
         "speed": 65,
         "special": 50
     }),
-    moves=("tackle", "ember")
+    moves=("tackle", "ember", "tail wip")
 )
 SquirtleBase = PokemonBase(
     pokedex_number=7,
@@ -171,7 +297,7 @@ SquirtleBase = PokemonBase(
         "speed": 43,
         "special": 50
     }),
-    moves=("tackle", "water gun")
+    moves=("tackle", "water gun", "growl")
 )
 RattataBase = PokemonBase(
     pokedex_number=19,
