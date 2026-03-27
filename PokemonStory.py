c621fd9ea1422204349bc839aa437eee508e7d30
Alberto De Davide <alberto.deda.01@gmail.com>
2026-03-27 17:16:20 +0100

    Assignment - 2 Working on FSM story

---
+++ PokemonStory.py
@@ -0,0 +1,16 @@
+import FSM
+
+story = FSM.FiniteStateMachine()
+
+CreateCharacter = FSM.State("CreateCharacter")
+Story = FSM.State("Story")
+Exit = FSM.State("Exit")
+PokemonStore = FSM.State("PokemonStore")
+PokemonCenter = FSM.State("PokemonCenter")
+Explore = FSM.State("Explore")
+Battle = FSM.State("Battle")
+
+
+
+
+
