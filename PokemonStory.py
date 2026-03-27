d091ae3484642387bbfe0efab4d4352d528e4a01
Alberto De Davide <alberto.deda.01@gmail.com>
2026-03-28 00:16:34 +0100

    Assignment - 2 Working on FSM all state transition are fine

--- PokemonStory.py
+++ PokemonStory.py
@@ -60,14 +60,102 @@ def BeginStory():
 
 
 def KeepPlaying(GameEngine):
-    print("Where do you want to go?")
-    GameEngine.draw(show_current_state=True, node_size = 3000, arrowsize = 10)
-    '''
-    for states in GameEngine.G.nodes.data():
-        print(states.name,"\n")
-
+    #GameEngine.draw(show_current_state=True, node_size = 3000, arrowsize = 10)
+    print(f"\nYou are in {GameEngine.state.name}")
+    print("Where do you want to go? (Possible destinations)")
+    next_states = list(GameEngine.G.successors(GameEngine.state))
+    for i,n_state in enumerate(next_states):
+        print("--> ",n_state.name)
     choice = (input("> "))
-    '''
+    while 1==1:
+        match choice:
+
+            case "CreateCharacter":
+                print("Can't go here!")
+            case "Story":
+                Story_state = None
+                for s in next_states:
+                    if s.name == "Story":
+                        Story_state = s
+                        break
+                if Story_state:
+                    GameEngine.do_transition(Story_state)
+                    GameEngine.eval_current()
+                else:
+                    print("Can't go here!")
+
+            case "PokemonStore":
+                PokemonStore_state = None
+                for s in next_states:
+                    if s.name == "PokemonStore":
+                        PokemonStore_state = s
+                        break
+                if PokemonStore_state:
+                    GameEngine.do_transition(PokemonStore_state)
+                    GameEngine.eval_current()
+                else:
+                    print("Can't go here!")
+
+            case "PokemonCenter":
+                PokemonCenter_state = None
+                for s in next_states:
+                    if s.name == "PokemonCenter":
+                        PokemonCenter_state = s
+                        break
+                if PokemonCenter_state:
+                    GameEngine.do_transition(PokemonCenter_state)
+                    GameEngine.eval_current()
+                else:
+                    print("Can't go here!")
+
+            case "Explore":
+                Explore_state = None
+                for s in next_states:
+                    if s.name == "Explore":
+                        Explore_state = s
+                        break
+                if Explore_state:
+                    GameEngine.do_transition(Explore_state)
+                    GameEngine.eval_current()
+                else:
+                    print("Can't go here!")
+
+            case "Exit":
+                Exit_state = None
+                for s in next_states:
+                    if s.name == "Exit":
+                        Exit_state = s
+                        break
+                if Exit_state:
+                    GameEngine.do_transition(Exit_state)
+                    GameEngine.eval_current()
+                    break
+                else:
+                    print("Can't go here!")
+
+            case "Battle":
+                Battle_state = None
+                for s in next_states:
+                    if s.name == "Battle":
+                        Battle_state = s
+                if Battle_state:
+                    GameEngine.do_transition(Battle_state)
+                    GameEngine.eval_current()
+                else:
+                    print("Can't go here!")
+
+            case _:
+                print("Can't go here!")
+
+        print(f"\nYou are in {GameEngine.state.name}")
+        print("Where do you want to go? (Possible destinations)")
+        next_states = list(GameEngine.G.successors(GameEngine.state))
+        for i, n_state in enumerate(next_states):
+            print("--> ", n_state.name)
+        choice = (input("> "))
+
+
+
 
 
 
