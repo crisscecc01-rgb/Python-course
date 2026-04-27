c049ba1cbe8b474a3ba0c5c72eb055ccaa653189
CrissCecc <crisscecc.01@gmail.com>
2026-04-27 08:59:23 +0200

    assignment 5 - ML implemented

--- Database/PokemonDatabase.py
+++ Database/PokemonDatabase.py
@@ -8,11 +8,37 @@ dir_js = os.path.dirname(os.path.abspath(__file__))
 
 with open(os.path.join(dir_js, "pokemons.json"), "r", encoding="utf-8") as f:
     pokemons_data = json.load(f)
-
+'''
 def pick_moves(pokemon_types, n=4):
     allowed_types = set(t for t in pokemon_types) | {"normal"}
     pool = MoveList_df[MoveList_df["type"].isin(allowed_types) & MoveList_df["power"].notna() & MoveList_df["accuracy"].notna()]
     return tuple(random.sample(list(pool.index), min(n, len(pool))))
+'''
+
+def pick_moves(pokemon_types, n=4):
+    allowed_types = set(t for t in pokemon_types) | {"normal"}
+
+    pool = MoveList_df[
+        MoveList_df["type"].isin(allowed_types)
+        & MoveList_df["power"].notna()
+        & MoveList_df["accuracy"].notna()
+    ]
+
+    stab_pool = MoveList_df[
+        MoveList_df["type"].isin(t for t in pokemon_types)
+        & MoveList_df["power"].notna()
+        & MoveList_df["accuracy"].notna()
+    ]
+
+    num_stab = min(2, len(stab_pool))
+    stab_moves = random.sample(list(stab_pool.index), num_stab)
+
+    remaining = n - num_stab
+    other_pool = pool.drop(stab_moves, errors="ignore")
+
+    other_moves = random.sample(list(other_pool.index), min(remaining, len(other_pool)))
+    return tuple(stab_moves + other_moves)
+
 
 pokemon_records = []
 
